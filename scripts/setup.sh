4c3bed4030a40ce66dbdcac5ebeb04ea5c56443b
Brian Fulton-Howard <fultonh1@gmail.com>
2022-07-13 18:48:26 -0400

    bugfixes

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,46 +3,62 @@
 set -euo pipefail # STRICT MODE
 
 shelltype=$(basename $SHELL)
-if echo $SHELL | grep -q "fish"; then
+if [[ "$shelltype" == "fish" ]]; then
   SHELLCONF=$HOME/.config/fish/config.fish
   shelltype=
-elif echo $SHELL | grep -q "bash"; then
+elif [[ "$shelltype" == "bash" ]]; then
   SHELLCONF=$HOME/.bash_profile
-elif echo $SHELL | grep -q "zsh"; then
+elif [[ "$shelltype" == "zsh" ]]; then
   SHELLCONF=$HOME/.zshrc
 else
   SHELLCONF="other"
 fi
 
 if echo $HOME | grep -q "^/hpc/users/"; then
-  if [ -z ${CONDA_PREFIX+x} ]
   curl https://raw.githubusercontent.com/marcoralab/lab_operations/main/config_files/.condarc > $HOME/.condarc
   echo -e "\n\nconda config --set auto_activate_base false\n" >> $HOME/.condarc
   mkdir -p /sc/arion/work/$USER/conda/envs
   mkdir -p /sc/arion/work/$USER/conda/pkgs
-  conda_prefix="/sc/arion/work/$USER/conda/miniconda3"
-  wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-  bash Miniconda3-latest-Linux-x86_64.sh -bp $conda_prefix
-  rm Miniconda3-latest-Linux-x86_64.sh
-  $conda_prefix/bin/conda init $shelltype
-  if [[ $SHELLCONF == "other" ]]; then
-    echo "Unknown shell. Please follow guide to setup anaconda"
-    exit 1
+  
+  newconda=0
+  if ! conda --help &> /dev/null; then
+    export newconda=1
+    conda_prefix="/sc/arion/work/$USER/conda/miniconda3"
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    bash Miniconda3-latest-Linux-x86_64.sh -bp $conda_prefix
+    rm Miniconda3-latest-Linux-x86_64.sh
+    $conda_prefix/bin/conda init $shelltype
+    if [[ $SHELLCONF == "other" ]]; then
+      echo "Unknown shell. Please rerun this script to continue."
+      exit 1
+    fi
+    source $SHELLCONF
+  else
+    conda deactivate
   fi
-  source $SHELLCONF
-  conda install -y mamba
-  mamba init
-  mamba create -y -n py3.10 python=3.10 snakemake ipython ipdb jupyterlab \
-    biopython visidata miller flippyr mamba gh git code-server vim radian \
-    pygit2 powerline-status r-base=4.1 r-essentials r-languageserver
 
+  if ! mamba --help &> /dev/null; then
+    conda install -y mamba
+  elif [[ $newconda -eq 0 ]]; then
+    mamba update mamba
+  fi
+  
+  mamba init
+
+  if conda env list | grep -qE "^py3.10\s+"; then
+    mamba create -y -n py3.10 python=3.10 snakemake ipython ipdb jupyterlab \
+      biopython visidata miller flippyr mamba gh git code-server vim radian \
+      pygit2 powerline-status r-base=4.1 r-essentials r-languageserver
+  fi
+  
   printf "\n\n conda activate py3.10\n" >> $SHELLCONF
+  mamba activate py3.10
 else
   lcl_pkgs="snakemake ipython ipdb jupyterlab biopython \
     visidata miller flippyr pygit2 powerline-status"
   newconda=0
   if ! conda --help &> /dev/null; then
-    newconda=1
+    export newconda=1
     curl https://raw.githubusercontent.com/marcoralab/lab_operations/main/config_files/local.condarc > $HOME/.condarc
     if [ "$(uname)" == "Darwin" ]; then
       conda_inst="Miniconda3-latest-MacOSX-x86_64.sh"
@@ -53,21 +69,25 @@ else
     bash $conda_inst -b
     rm $conda_inst
     $HOME/miniconda3/bin/conda init $shelltype
-    if [[ $SHELLCONF == "other" ]]; then
-      echo "Unknown shell. Please follow guide to setup anaconda"
+    if ! [[ $SHELLCONF == "other" ]]; then
+      echo "Unknown shell. Please rerun this script to continue."
       exit 1
     fi
     source $SHELLCONF
+    conda install -y mamba
+    mamba install -y python=3.10 $lcl_pkgs
   fi
-  conda install -y mamba
-  
+
   if [[ $newconda -eq 0]]; then
+    if ! mamba --help &> /dev/null; then
+      conda install -y mamba
+    fi
     mamba install -y $lcl_pkgs
     mamba update -y mamba conda
     mamba update -y $lcl_pkgs
-  else
-    mamba install -y python=3.10 $lcl_pkgs
   fi
+
+  mamba activate
 fi
 
 export SETUP_SCRIPT=1
