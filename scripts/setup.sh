ceb9012419d6b8c80f61940fc9151ff0c40827df
Brian Fulton-Howard <fultonh1@gmail.com>
2022-07-27 15:01:30 -0400

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -35,11 +35,11 @@ if echo $HOME | grep -q "^/hpc/users/"; then
       echo "Unknown shell. Please rerun this script to continue."
       exit 1
     elif [[ $SHELLCONF == "bash" ]]; then
-      export PS1='[\u@\h \W]\$'
+      export PS1=''
       source $SHELLCONF
     else
       $conda_prefix/bin/conda init bash
-      export PS1='[\u@\h \W]\$'
+      export PS1=''
       source $HOME/.bash_profile
     fi
   else
