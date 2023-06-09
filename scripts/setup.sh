3b73c3832231c6069311b4a34aea78fd35ba464c
Brian Fulton-Howard <fultonh1@gmail.com>
2023-06-09 14:55:13 -0400

    try sourcing bashrc

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -113,7 +113,7 @@ else
       source $SHELLCONF
     else
       $HOME/mambaforge/bin/conda init bash
-      source $HOME/.bash_profile
+      source $HOME/.bashrc
     fi
     mamba update -y mamba conda
     mamba install -y python=$pyversion $lcl_pkgs
